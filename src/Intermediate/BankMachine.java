05c791d0565891a5862c837618990183a3a91bd5
Farhaad Wasim <farhaad.wasim@goolemai.com>
2017-12-14 20:25:01 +0000

    Added while loop to select options

--- src/Intermediate/BankMachine.java
+++ src/Intermediate/BankMachine.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Intermediate;
 
 import java.util.Scanner;
@@ -13,19 +8,49 @@ public class BankMachine {
     private static String correctPass = "password";
     private static int tries = 0;
     private static boolean accountLocked = false;
-    private static double currentBalance = 0;
+    private static double currentBalance = 100.00;
     private int[] transactions = new int[10];
   
-   
+    static Scanner sc = new Scanner(System.in);
+           
     public static void main(String[] args) {
-
-        // login to account
-        login();
-        showMenu();
+       System.out.println("Welcome to the bank machine!");
+       
+       login();
+       showMenu();
+       
+       System.out.print("select an option: ");
+       int choice = sc.nextInt();
+       
+       while(true){
+           if(choice == 1){
+               System.out.println("Your balance is " + currentBalance);
+               break;
+           }else if(choice == 2){
+               System.out.println("How much do you want to withdraw? ");
+               int amount = sc.nextInt();
+               if(amount > currentBalance){
+                   System.out.println("You do not have enough money to widthdraw");
+               }else{
+                   currentBalance = currentBalance - amount;
+                   System.out.println("Your balance is " + currentBalance);
+               }
+               break;
+           }else if(choice == 3){
+               System.out.println("You selected 3!");
+               break;
+           }else if(choice == 4) {
+               System.out.println("Thank you for using bank machine");
+               break;
+           }else{
+               System.out.print("Incorrect option. Try again: ");
+               choice = sc.nextInt();
+           }
+       }
     }
 
     public static boolean login() {
-        Scanner sc = new Scanner(System.in);
+
         boolean loggedIn = false;
         do {
             System.out.print("Enter Username: ");
@@ -40,10 +65,9 @@ public class BankMachine {
                 System.out.println("Incorrect username or password. Try again");
                 tries++;
             }
-            if(tries >= 3){
-                // add functions to reset password 
+            if(tries == 3){
                 accountLocked = true;
-            }
+            }   
         }while(!loggedIn);
     
         return loggedIn;
@@ -61,17 +85,16 @@ public class BankMachine {
     }
 
     public static void showMenu() {
-        Scanner sc = new Scanner(System.in);
+        System.out.println("\n");
         System.out.println("==== SimpleATM =====");
         System.out.println("");
         System.out.println("========================");
         System.out.println("| [1]  Check Balance   |");
         System.out.println("| [2]  Withdrawal      |");
-        System.out.println("| [2]  Transactions    |");
+        System.out.println("| [3]  Transactions    |");
         System.out.println("| [4]  Exit            |");
         System.out.println("========================");
         System.out.println("");
-
     }
 
 }

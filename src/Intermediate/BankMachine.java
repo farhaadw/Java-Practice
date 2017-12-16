2f95ae86b15c916152ccabe804e41aef451888c7
Farhaad Wasim <farhaad.wasim@goolemai.com>
2017-12-16 01:41:18 +0000

    Added initial methods to add transactions

--- src/Intermediate/BankMachine.java
+++ src/Intermediate/BankMachine.java
@@ -1,6 +1,12 @@
 package Intermediate;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class BankMachine {
 
@@ -9,14 +15,15 @@ public class BankMachine {
     private static int tries = 0;
     private static boolean accountLocked = false;
     private static double currentBalance = 100.00;
-    private int[] transactions = new int[10];
+    private static Map<String, Double> transactions = new HashMap<String, Double>();
+    // keep the bank machine running 
+    private static boolean runBank = true;
            
     static Scanner sc = new Scanner(System.in);
            
     public static void main(String[] args) {
        System.out.println("Welcome to the bank machine!");
-       
-       boolean runBank = true;
+      
        login();
        showMenu();
        
@@ -24,11 +31,8 @@ public class BankMachine {
            System.out.print("select an option: ");
            int choice = sc.nextInt();
            if(choice == 1){
-               System.out.println("Your balance is " + currentBalance);
-               if(anotherOption()){
-                   continue;
-               }
-               runBank = false;
+              System.out.println("Your balance is " + currentBalance);
+              anotherOption();
            }else if(choice == 2){
                System.out.println("How much do you want to withdraw? ");
                int amount = sc.nextInt();
@@ -36,18 +40,13 @@ public class BankMachine {
                    System.out.println("You do not have enough money to widthdraw");
                }else{
                    currentBalance = currentBalance - amount;
-                   System.out.println("Your balance is " + currentBalance);
+                   System.out.println("Your balance is now " + currentBalance);
+                   addTransaction(amount);;
                }
-               if(anotherOption()){
-                   continue;
-               }
-               runBank = false;
+               anotherOption();
            }else if(choice == 3){
-               System.out.println("You selected 3!");
-               if(anotherOption()){
-                   continue;
-               }
-               runBank = false;
+               getTransactions();
+               anotherOption();
            }else if(choice == 4) {
                System.out.println("Thank you for using SimpleATM");
                runBank = false;
@@ -105,7 +104,21 @@ public class BankMachine {
         return false;
     }
     
-    public static boolean anotherOption(){
+    public static void addTransaction(double transaction){
+        SimpleDateFormat dt1 = new SimpleDateFormat("dd-MM-yyyy");
+        transactions.put(dt1.format(new Date()), transaction);
+    }
+    
+    public static void getTransactions(){
+      Set set = transactions.entrySet();
+      Iterator iterator = set.iterator();
+      while(iterator.hasNext()) {
+         Map.Entry value = (Map.Entry)iterator.next();
+         System.out.println("Date: " + value.getKey() + " Withdraw " + value.getValue());
+      }
+    }
+    
+    public static void anotherOption(){
           System.out.print("Another option? (Y/N) ");
           String option = sc.next();
           while((!option.equalsIgnoreCase("y")) && (!option.equalsIgnoreCase("n"))){
@@ -113,10 +126,11 @@ public class BankMachine {
              option = sc.next();
           }
           if(option.equalsIgnoreCase("y")){
-              return true;
+              runBank = true;
+              return;
           }
           System.out.println("Thank you for using SimpleATM");
-          return false;
+          runBank = false;
     }
     
     public static void showMenu() {
@@ -131,5 +145,4 @@ public class BankMachine {
         System.out.println("========================");
         System.out.println("");
     }
-
 }
